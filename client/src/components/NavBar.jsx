a8276e5e4c1af157270a523c3b46c169a68eef88
Nel <nel.renaudin@gmail.com>
2024-01-14 11:35:50 +0100

    add icons in NavBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom"
+import {
+  PiCaretDownBold,
+  PiMagnifyingGlass,
+  PiHeart,
+  PiBag,
+  PiUser,
+} from "react-icons/pi"
 import { useAuth } from "../providers/authProvider"
-import logo from "../assets/seldo.png"
+import logo from "../assets/seldo.svg"
 
 const NavBar = () => {
   const { user, setUser } = useAuth()
@@ -13,36 +20,49 @@ const NavBar = () => {
     <nav className="px-12 border-b border-slate-200">
       <div className="h-24 flex justify-between items-center">
         <Link to="/">
-          <img src={logo} alt="Seldo" />
+          <img src={logo} alt="Seldo" className="h-6" />
         </Link>
 
-        <ul className="flex items-center gap-3">
+        <ul className="flex items-center gap-6">
+          <li className="flex items-center gap-1">
+            Categories
+            <PiCaretDownBold className="text-gray-400" />
+          </li>
+          <li className="flex items-center gap-1">
+            Brands
+            <PiCaretDownBold className="text-gray-400" />
+          </li>
+          <li>
+            <Link to="/catalog">Latest</Link>
+          </li>
           {user && user.isAdmin && (
             <li>
-              <Link
-                to="/dashboard"
-                className="p-3 text-slate-600 hover:text-black"
-              >
-                Dashboard
-              </Link>
+              <Link to="/dashboard">Dashboard</Link>
             </li>
           )}
-          {user ? (
-            <li>
-              <button
-                onClick={logout}
-                className="p-3 text-slate-600 hover:text-black"
-              >
-                Logout
-              </button>
-            </li>
-          ) : (
-            <li>
-              <Link to="/login" className="p-3 text-slate-600 hover:text-black">
-                Account
-              </Link>
-            </li>
-          )}
+        </ul>
+
+        <ul className="flex items-center gap-3">
+          <li>
+            <button className="btn-icon">
+              <PiMagnifyingGlass />
+            </button>
+          </li>
+          <li>
+            <Link to="/favorites" className="btn-icon">
+              <PiHeart />
+            </Link>
+          </li>
+          <li>
+            <button className="btn-icon">
+              <PiBag />
+            </button>
+          </li>
+          <li>
+            <Link to="/login" className="btn-icon">
+              <PiUser />
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
